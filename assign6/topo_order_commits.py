d7809b89fef57c90cc92b43707c837d311a1adc6
Paul Zhang <paulz@ucla.edu>
2022-11-14 23:43:52 -0800

    Assign6: Find .git dir in current or higher dir

---
+++ assign6/topo_order_commits.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+
+class GitRepo:
+    """
+    Does fancy things with a git repository.
+    """
+
+    def __init__(self):
+        """
+        Initialize by finding .git directory in current or higher directories. Exits if none found.
+        """
+
+        root = os.sep
+        self.dir = root
+
+        while True:
+            if os.path.isdir(".git"):
+                self.dir = f"{os.getcwd()}/.git"
+                return
+
+            if os.getcwd() == root:
+                print("Not inside a Git repository", file=sys.stderr)
+                sys.exit(1)
+
+            try:
+                os.chdir("..")
+            except OSError as error:
+                print(f"bruh something went wrong: {error}")
+
+    def location(self):
+        """
+        Directory of git repository.
+        """
+
+        return self.dir
+
+
+if __name__ == "__main__":
+    repo = GitRepo()
+    print(repo.location())
