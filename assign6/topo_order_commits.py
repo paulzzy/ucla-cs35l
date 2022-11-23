6087fbaee2fe57537991af152a824ef3457d232c
Paul Zhang <paulz@ucla.edu>
2022-11-22 20:40:33 -0800

    Format to fit line length of 79

--- assign6/topo_order_commits.py
+++ assign6/topo_order_commits.py
@@ -5,8 +5,8 @@ To verify my implementation doesn't use other commands, I ran `strace -f -e
 execve -o lmao.trace pytest` (with the testing environment properly set up) and
 then examined the resultant lmao.trace file.
 
-If the only line containing "execve" indicates that `pytest` was called, then no
-other commands were called and my implementation is spec-compliant.
+If the only line containing "execve" indicates that `pytest` was called, then
+no other commands were called and my implementation is spec-compliant.
 """
 
 import os
@@ -42,8 +42,8 @@ class CommitNode:
 
     def object_path(self) -> str:
         """
-        Assumes the current working directory is .git, returns the relative path
-        of the object file associated with this commit.
+        Assumes the current working directory is .git, returns the relative
+        path of the object file associated with this commit.
         """
 
         return f"objects/{self.commit_hash[0:2]}/{self.commit_hash[2:]}"
@@ -109,14 +109,16 @@ class GitRepo:
             for branch in branches:
                 maybe_head_location: str = f"{self.dir}/{REFS_HEADS}/{branch}"
 
-                # Branch name can have "/", causing its commit hash file inside .git
-                # to be inside nested directories. For example, for branch name
-                # a/b/c the commit hash file is located at .git/refs/heads/a/b/c.
+                # Branch name can have "/", causing its commit hash file inside
+                # .git to be inside nested directories. For example, for branch
+                # name a/b/c the commit hash file is located at
+                # .git/refs/heads/a/b/c.
                 #
                 # Keep traversing down until a file is encountered.
                 while not os.path.isfile(maybe_head_location):
                     maybe_head_location = (
-                        f"{maybe_head_location}/{os.listdir(maybe_head_location)[0]}"
+                        f"{maybe_head_location}/"
+                        f"{os.listdir(maybe_head_location)[0]}"
                     )
 
                 head_location: str = maybe_head_location
@@ -128,12 +130,15 @@ class GitRepo:
 
         def populate_tree(heads: list[CommitNode]) -> list[CommitNode]:
             """
-            Uses multiple depth-first searches, each starting from a branch HEAD
-            commit. Returns a list of root commits (those with no parents).
+            Uses multiple depth-first searches, each starting from a branch
+            HEAD commit. Returns a list of root commits (those with no
+            parents).
             """
 
             commits: list[CommitNode] = []
-            existing: dict[str, CommitNode] = {head.commit_hash: head for head in heads}
+            existing: dict[str, CommitNode] = {
+                head.commit_hash: head for head in heads
+            }
 
             # Since two branches can have the same head commit, avoid including
             # duplicate commits by constructing from `existing`
@@ -156,7 +161,11 @@ class GitRepo:
                         f"{self.dir}/{current.object_path()}",
                         mode="rb",
                     ) as commit_object:
-                        text = zlib.decompress(commit_object.read()).decode().split()
+                        text = (
+                            zlib.decompress(commit_object.read())
+                            .decode()
+                            .split()
+                        )
 
                         for i, token in enumerate(text):
                             if token == "parent":
@@ -165,9 +174,9 @@ class GitRepo:
                             if token == "author":
                                 # Reached end of parent commits metadata, after
                                 # which is other metadata and then the commit
-                                # message. Since the message could contain "parents"
-                                # on one line, which would break parsing, it's best
-                                # to exit early.
+                                # message. Since the message could contain
+                                # "parents" on one line, which would break
+                                # parsing, it's best to exit early.
                                 break
 
                     for parent_hash in parent_hashes:
@@ -187,7 +196,8 @@ class GitRepo:
                 except OSError as error:
                     print(
                         f"{os.path.basename(__file__)}: "
-                        f"This repository may be using packfiles, which is not supported"
+                        f"This repository may be using packfiles, "
+                        f"which is not supported"
                         f": {error}",
                         file=sys.stderr,
                     )
@@ -241,10 +251,10 @@ class GitRepo:
         for commit in self.topo_sorted_commits:
             current_line: str = f"{commit.commit_hash}"
 
-            # When current commit is not a parent of the previous, add a "sticky
-            # end" and "sticky start" to indicate how to reconstruct the commit
-            # graph. The sticky parts include information on the previous
-            # commit's parents and the current commit's children.
+            # When current commit is not a parent of the previous, add a
+            # "sticky end" and "sticky start" to indicate how to reconstruct
+            # the commit graph. The sticky parts include information on the
+            # previous commit's parents and the current commit's children.
             #
             # Format of sticky end:
             # ```
@@ -257,18 +267,20 @@ class GitRepo:
             # =child_1 child_2 ... child_n
             # ```
             if prev and prev not in commit.children:
-                sticky_end: str = (
-                    f"{' '.join([parent.commit_hash for parent in prev.parents])}="
-                )
-                sticky_start: str = (
-                    f"\n\n="
-                    f"{' '.join([child.commit_hash for child in commit.children])}\n"
-                )
+                prev_parents: list[str] = [
+                    parent.commit_hash for parent in prev.parents
+                ]
+                sticky_end: str = f"{' '.join(prev_parents)}="
+                curr_children: list[str] = [
+                    child.commit_hash for child in commit.children
+                ]
+                sticky_start: str = f"\n\n=" f"{' '.join(curr_children)}\n"
                 current_line = f"{sticky_end}{sticky_start}{current_line}"
 
             if commit.commit_hash in self.branch_heads:
                 branch_names = sorted(
-                    self.branch_heads[commit.commit_hash], key=lambda branch: branch
+                    self.branch_heads[commit.commit_hash],
+                    key=lambda branch: branch,
                 )
                 current_line = f"{current_line} {' '.join(branch_names)}"
 
